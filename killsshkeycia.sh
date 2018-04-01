7f80e449a1476eca005403cdc6bc6e852b4663d6
WilsonBillkia <32509684+WilsonBillkia@users.noreply.github.com>
2018-04-01 20:26:30 +0100

    Update killsshkeycia.sh

--- killsshkeycia.sh
+++ killsshkeycia.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #command to remove keys from .ssh/known_hosts during testing
-ssh-keygen -f "/home/network/.ssh/known_hosts" -R 192.168.33.16
+ssh-keygen -f "~/.ssh/known_hosts" -R 192.168.33.16
