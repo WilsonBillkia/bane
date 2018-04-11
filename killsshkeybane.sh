0c090274cf8bd4ac04bb47f9f411396326ebc294
WilsonBillkia <32509684+WilsonBillkia@users.noreply.github.com>
2018-04-11 05:57:58 +0100

    Update killsshkeybane.sh

--- killsshkeybane.sh
+++ killsshkeybane.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#command to remove keys from .ssh/known_hosts during testing
+#handy alias to remove keys from .ssh/known_hosts during testing on machines with ssh strict checking enabled  
 ssh-keygen -f "~/.ssh/known_hosts" -R 192.168.33.15
